d4251be45e85faa4dd26c4f38427366c2a3e3ddf
bitcoinaudio <bitcoinaudio@gmail.com>
2024-11-09 15:42:50 -0500

    003

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react')) return 'react-vendor'
+            if (id.includes('lodash') || id.includes('axios')) return 'utility-vendor'
+            return 'vendor'
+          }
+          // Group app-specific code together
+          return 'app'
+        },
+      },
+    },
+  },
 })
