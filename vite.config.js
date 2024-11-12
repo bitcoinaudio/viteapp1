96cbd2ffeecf2b2b676916c0f0395a539407a955
bitcoinaudio <bitcoinaudio@gmail.com>
2024-11-12 00:19:13 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('react')) return 'react-vendor'
-            if (id.includes('lodash') || id.includes('axios')) return 'utility-vendor'
-            return 'vendor'
-          }
-          // Group app-specific code together
-          return 'app'
-        },
-      },
-    },
-  },
+ 
 })
