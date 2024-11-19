d06bee382dd6b64816a8a816dd0b75d9e46ed290
bitcoinaudio <bitcoinaudio@gmail.com>
2024-11-19 16:33:00 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './AppBlood.jsx'
+import App from './AppBlack.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
